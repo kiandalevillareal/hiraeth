a77194cb48cba111e7af2648b83529a1131a3d2e
kiandalevillareal <kiandalevillareal@gmail.com>
2023-07-28 13:29:20 +0800

    add: sections

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ popupImage.addEventListener("click", function(){
     document.querySelector(".popup-image").style.display = "none";
 });
 
+const navLinks = document.querySelectorAll('.nav-link');
+const contentSections = document.querySelectorAll('.content');
+
+function handleNavLinkClick(e) {
+    e.preventDefault();
+
+    contentSections.forEach((section) => {
+        section.style.display = 'none';
+    });
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.style.display = 'block';
+    targetSection.classList.add('active');
+}
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', handleNavLinkClick);
+});
