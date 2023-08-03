1bdf2d25e64cb57cac79d069ae7a5a7c98d137b7
kiandalevillareal <kiandalevillareal@gmail.com>
2023-08-03 11:33:49 +0800

    added some changes

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    // // FOR THE NAV BAR
-    // const navLinks = document.querySelectorAll('.nav-link');
-    // const contentSections = document.querySelectorAll('.content-section');
-
-    // const onClickNavLinks = (event) => {
-    //     event.preventDefault(); 
-
-    //     contentSections.forEach((section) => {
-    //         section.style.display = 'none';
-    //     });
-
-    //     navLinks.forEach((link) => {
-    //         link.style.backgroundColor = 'white ';
-    //     });
-
-    //     const targetSectionId = event.target.getAttribute('href');
-    //     const targetSection = document.querySelector(targetSectionId);
-    //     if (targetSection) {
-    //         targetSection.style.display = 'block';
-    //         event.target.style.backgroundColor = '#aed6d3';
-    //         }
-    // };
-
-    // navLinks.forEach((link) => {
-    //     link.addEventListener('click', onClickNavLinks);
-    // });
-
-    // FOR THE IMAGES
-    
     const images = document.querySelectorAll('.gallery img');
     images.forEach(e => {
         e.onclick = () =>{
